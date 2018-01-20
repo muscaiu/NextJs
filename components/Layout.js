c2318ecff8c661f11d629212382bd79b1cd4da6a
Cris <muscaiu@yahoo.com>
2018-01-20 23:48:14 +0200

    added a bootstrap nav

--- components/Layout.js
+++ components/Layout.js
@@ -5,10 +5,14 @@ const Layout = (props) => (
   <div>
     <Head>
       <title>BitzPrice</title>
-      <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css"/>
-    </Head> 
+      <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css" />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </Head>
     <Navbar />
-    {props.children}
+    <div className="container">
+      {props.children}
+    </div>
   </div>
 );
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,27 @@
 import Link from 'next/link'
 
 const Navbar = () => (
-  <div>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary" >
+    <a className="navbar-brand" href="/">BitzPrice</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-    <ul>
-      <li><Link href='/'><a>Home</a></Link></li>
-      <li><Link href='/about'><a>About</a></Link></li>
-    </ul>
-    <style jsx>{`
-    ul{
-      background: #333;
-      color: #fff;
-      list-style: none;
-      display: flex;
-    }
-
-    ul li{
-      font-size: 18px;
-      margin-right: 20px;
-    }
-
-    ul li a{
-      color: #fff;
-      text-decoration: none;
-    }
-    `}</style>
-  </div>
+    <div className="collapse navbar-collapse" id="navbarColor01">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <Link href="/" >
+            <a className="nav-link">Home <span className="sr-only">(current)</span></a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/about" >
+          <a className="nav-link">About</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 )
 
 export default Navbar;
